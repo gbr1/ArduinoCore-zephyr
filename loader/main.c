3aecf50341c7a05b7b42df1eea787f08522fdfcd
Martino Facchin <m.facchin@arduino.cc>
2026-01-24 16:55:12 +0800

    gertrude: switch to real hw

--- loader/main.c
+++ loader/main.c
@@ -151,7 +151,7 @@ static int loader(const struct shell *sh) {
 		// This is not a valid sketch, but try to start a shell anyway
 	}
 
-#if defined(CONFIG_BOARD_ARDUINO_UNO_Q)
+#if defined(CONFIG_BOARD_ARDUINO_UNO_Q) || defined(CONFIG_BOARD_ARDUINO_GERTRUDE)
 	void matrixBegin(void);
 	void matrixEnd(void);
 	void matrixPlay(uint8_t *buf, uint32_t len);
